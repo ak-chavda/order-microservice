eb2acc72cd255d69b8fa35f6b46d94042bac86ab
Amit Chavada <amit.chavada@crestron.com>
2022-03-05 11:25:17 +0530

    commiting req. files to order service

--- src/main/java/com/example/order/Strings.java
+++ src/main/java/com/example/order/Strings.java
@@ -1,6 +1,6 @@
 package com.example.order;
 
 public interface Strings {
-	public final static String GET_CART_DATA = "http://localhost:8888/api/cart/user1/";
-	public static final String CLEAN_CART = "http://localhost:8888/api/cart/removeAll/user1/";
+	public final static String GET_CART_DATA = "http://localhost:8080/api/cart/user1/";
+	public static final String CLEAN_CART = "http://localhost:8080/api/cart/removeAll/user1/";
 }
